de4a92e8a9747c6ec4a64fa6b2b0058273d8b1c7
Skrasilnikova <rray136@yandex.ru>
2023-08-21 22:20:22 +0300

    step 4_2_6

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url):
@@ -10,3 +11,15 @@ class BasePage():
     
     def open(self):
         self.browser.get(self.url)
+        
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+        
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
