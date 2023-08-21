b63175898f4702bf7cae3c02805a21ee3c42b45d
Skrasilnikova <rray136@yandex.ru>
2023-08-21 22:05:07 +0300

    step 4_2_4

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from .base_page import BasePage
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click() 
+
